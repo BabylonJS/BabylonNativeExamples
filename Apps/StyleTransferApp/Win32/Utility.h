b9e67b42967121c1fad2fc08d6c402956383c537
Sergio Ricardo Zerbetto Masson <sergioze@microsoft.com>
2023-01-12 14:02:55 -0300

    Renamed StyleTransferApp folder

--- Apps/StyleTransferApp/Win32/Utility.h
+++ Apps/StyleTransferApp/Win32/Utility.h
@@ -77,21 +77,6 @@ namespace Utility
     {
     }
 #endif
-
-    std::wstring UTF8ToWideString( const std::string& str );
-    std::string WideStringToUTF8( const std::wstring& wstr );
-    std::string ToLower(const std::string& str);
-    std::wstring ToLower(const std::wstring& str);
-    std::string GetBasePath(const std::string& str);
-    std::wstring GetBasePath(const std::wstring& str);
-    std::string RemoveBasePath(const std::string& str);
-    std::wstring RemoveBasePath(const std::wstring& str);
-    std::string GetFileExtension(const std::string& str);
-    std::wstring GetFileExtension(const std::wstring& str);
-    std::string RemoveExtension(const std::string& str);
-    std::wstring RemoveExtension(const std::wstring& str);
-
-
 } // namespace Utility
 
 #ifdef ERROR
